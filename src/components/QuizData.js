9d442bcaf011b37fd1abd1a2f5ba8a1089d8ffb7
PRATHAM GUPTA <PRATHAM17GUPTA@GMAIL.COM>
2022-03-29 02:13:58 +0530

    first commit

---
+++ src/components/QuizData.js
@@ -0,0 +1,26 @@
+export const QuizData = [
+    {
+        id: 0,
+        question: `Who is the CEO of Trianz?`,
+        options: [`Narendra Modi`, `Yogi Adityanath`, `Sri Manchala`, `Amit Shah`],
+        answer: `Sri Manchala`
+    },
+    {
+        id: 1,
+        question: `When was Trianz founded?`,
+        options: [`1998`, `1999`, `2000`, `2001`],
+        answer: `2000`
+    },
+    {
+        id: 2,
+        question: `Where is the Headquarter of Trianz?`,
+        options: [`Mumbai`, `Santa Clara`, `Hyderabad`, `Sydney`],
+        answer: `Santa Clara`
+    },
+    {
+        id: 3,
+        question: `Who is the Presidentof Trianz?`,
+        options: [`Narendra Modi`, `Yogi Adityanath`, `Sri Manchala`, `Amit Shah`],
+        answer: `Sri Manchala`
+    }
+]
